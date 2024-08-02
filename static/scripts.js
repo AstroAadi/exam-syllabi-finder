7736aa9263eaf64c4530373356a7dee56db4435e
Aditya Gupta <aadigupta2946@gmail.com>
2024-08-02 12:51:54 +0530

    Update Frontend to use backend url

--- static/scripts.js
+++ static/scripts.js
@@ -1,34 +1,3 @@
-//document.addEventListener('DOMContentLoaded', fetchPopularExams);
-//
-//async function fetchPopularExams() {
-//    const popularExamsList = document.getElementById('popular-exams-list');
-//    const response = await fetch('/popular-exams');
-//    const popularExams = await response.json();
-//
-//    popularExams.forEach(exam => {
-//        const li = document.createElement('li');
-//        li.textContent = exam.name;
-//        popularExamsList.appendChild(li);
-//    });
-//}
-//
-//async function searchExams() {
-//    const query = document.getElementById('main-search').value;
-//    const resultsList = document.getElementById('search-results');
-//    resultsList.innerHTML = '';
-//
-//    const response = await fetch(`/search?query=${query}`);
-//    const results = await response.json();
-//
-//    results.forEach(result => {
-//        const li = document.createElement('li');
-//        const a = document.createElement('a');
-//        a.href = result.url;
-//        a.textContent = result.name;
-//        li.appendChild(a);
-//        resultsList.appendChild(li);
-//    });
-//}
 document.addEventListener("DOMContentLoaded", () => {
     fetchExams("");
 });
@@ -44,7 +13,7 @@ function searchExamHeader() {
 }
 
 function fetchExams(query) {
-    fetch(`/api/exams?q=${encodeURIComponent(query)}`)
+    fetch(`exam-syllabi-finder-backend-code-production.up.railway.app/api/exams?q=${encodeURIComponent(query)}`)
         .then(response => response.json())
         .then(exams => {
             const examList = document.getElementById("examList");
